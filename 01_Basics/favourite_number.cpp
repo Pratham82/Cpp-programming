f8284fa7718e4f6d0077383fb731e86c7e1eaed2
pratham82 <mali.prathamesh82@gmail.com>
2021-08-10 17:31:37 +0530

    Renamed folder

---
+++ 01_Basics/favourite_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+  int n;
+  cout << "Whats your favourite number ??";
+  cin >> n;
+  cout << "Wow nice, is also my favourite number !!" << endl;
+  cout << "No really " << n << " is really my favourite number";
+  cout << "Test" << endl;
+}
